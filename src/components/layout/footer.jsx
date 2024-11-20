3f68b12932b84ad0f1bd2ba4661b490f249d190d
Nguyễn Huy Phúc <huyphuc280503@gmail.com>
2024-11-20 14:54:53 +0700

    footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,5 +1,70 @@
+import {
+  Facebook,
+  Instagram,
+  Mail,
+  MapPinHouse,
+  Phone,
+  Twitter,
+} from "lucide-react";
+
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <div className="flex justify-around mt-7">
+      <div className="flex-col">
+        <img src="{Logo}" alt="NPC Store" />
+      </div>
+      <div className="flex flex-col items-start space-y-4">
+        <h5 className="text-lg font-semibold">Thông tin liên hệ</h5>
+        <dl className="space-y-2">
+          <dt className="flex items-center space-x-2">
+            <MapPinHouse className="" />
+            <span>Hà Nội</span>
+          </dt>
+          <dt className="flex items-center space-x-2">
+            <Phone className="" />
+            <span>0392280503</span>
+          </dt>
+          <dt className="flex items-center space-x-4">
+            <Facebook className="" />
+            <Twitter className="" />
+            <Instagram className="" />
+            <Mail className="" />
+          </dt>
+        </dl>
+      </div>
+
+      <div className="flex-col">
+        <h5 className="text-lg font-semibold">Dịch vụ</h5>
+        <br />
+        <ul>
+          <li>Vận chuyển hàng</li>
+          <li>Thanh toán tại nhà</li>
+          <li>Tư vấn mua hàng</li>
+          <li>Hướng dẫn sử dụng</li>
+        </ul>
+      </div>
+      <div className="flex-col">
+        <h5 className="text-lg font-semibold">Liên kết</h5>
+        <br />
+        <ul>
+          <li>Liên kết và góp ý</li>
+          <li>Câu hỏi thường gặp</li>
+          <li>Tuyển dụng</li>
+          <li>Hướng dẫn sử dụng</li>
+        </ul>
+      </div>
+      <div className="flex-col">
+        <h5 className="text-lg font-semibold">Hướng dẫn thanh toán</h5>
+        <br />
+        <ul>
+          <li>Hướng dẫn thành toán</li>
+          <li>Hướng dẫn mua hàng</li>
+          <li>Hướng dẫn sử dụng</li>
+          <li>Hướng dẫn cài đặt</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/layout/header-actions.jsx
+++ src/components/layout/header-actions.jsx
@@ -1,10 +1,18 @@
 import { Languages, ShoppingBag, User } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const HeaderActions = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-[18%] flex items-center justify-evenly">
-      <User size={24} strokeWidth={1.5} />
-      <ShoppingBag size={24} strokeWidth={1.5} />
+      <User
+        size={24}
+        strokeWidth={1.5}
+        className="cursor-pointer"
+        // onClick={() => navigate("/about")}
+      />
+      <ShoppingBag size={24} strokeWidth={1.5} className="cursor-pointer" />
       <Languages size={24} strokeWidth={1.5} />
     </div>
   );
