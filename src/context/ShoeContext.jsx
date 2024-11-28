33d0abb15e4ea0b2141778c302bbb933ca634f22
Nguyễn Huy Phúc <huyphuc280503@gmail.com>
2024-11-29 01:20:30 +0700

    chỉnh sửa shoeContext

--- src/context/ShoeContext.jsx
+++ src/context/ShoeContext.jsx
@@ -4,38 +4,9 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ShoeContext = createContext(null);
 
 export const ShoeContextProvider = ({ children }) => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [cartItems, setCartItems] = useState([]);
   const [shoeList, setShoeList] = useState([]);
-
-  const addToCart = async (itemId) => {
-    if (!cartItems[itemId]) {
-      setCartItems({
-        ...cartItems,
-        [itemId]: 1,
-      });
-    } else {
-      setCartItems((prev) =>
-        setCartItems({
-          ...prev,
-          [itemId]: prev[itemId] + 1,
-        })
-      );
-    }
-
-    if (token) {
-      await axios.post(
-        "http://localhost:8080/",
-        { itemId },
-        { headers: { token } }
-      );
-    }
-  };
-
-  useEffect(() => {
-    const token = localStorage.getItem("token") || "";
-    setToken(token);
-  }, []);
 
   const removeToken = () => {
     localStorage.removeItem("token");
