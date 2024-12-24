2984da6d585b6d859ccde90b8e8a44ffb18af443
Nguyễn Huy Phúc <huyphuc280503@gmail.com>
2024-12-24 18:28:53 +0700

    cập nhật components cho phụ kiện

---
+++ src/pageCategoryShoe/category-accessory/insole.jsx
@@ -0,0 +1,48 @@
+import ProductCard from "@/components/Home/ProductCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const Insole = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8080/products/categories/10/brands/13?page=0&limit=50"
+        );
+
+        const prod = res.data.payload.products;
+
+        setProducts(prod);
+      } catch {
+        setProducts([]);
+        toast.error("Lỗi khi lấy dữ liệu sản phẩm");
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (products.length === 0) {
+    return (
+      <div className="flex-1 flex items-center justify-center flex-col">
+        <img
+          src="/empty-product.png"
+          alt="No Product"
+          className="w-[192px] h-[192px]"
+        />
+        Hiện tại không có sản phẩm !!!
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-5">
+      {products.map((item, index) => (
+        <ProductCard key={index} data={item} />
+      ))}
+    </div>
+  );
+};
+
+export default Insole;
